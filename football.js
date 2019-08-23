c28086cc88825837d9d51cb89e6377a02badf5d0
vastex <37942727+vastex@users.noreply.github.com>
2019-08-23 23:34:06 +0200

    Update football.js

--- football.js
+++ football.js
@@ -4,11 +4,7 @@ Module.register("football", {
     getDom: function () {
         var element = document.createElement("div");
         element.className = "myContent";
-        element.innerHTML = "Hello, World! ";
-        var subElement = document.createElement("table");
-        subElement.innerHTML = "<table style='width:100%' id='standing'><tr><th>Heim</th><th>Gast</th><th>Ergebnis</th></tr><tr><td>BVB</td><td>Bayern</td><td>2:2</td></tr></table>";
-        subElement.id = "COUNT";
-        element.appendChild(subElement);
+        element.innerHTML = "<table style='width:100%' id='standing'><tr><th>Heim</th><th>Gast</th><th>Ergebnis</th></tr><tr><td>BVB</td><td>Bayern</td><td>2:2</td></tr></table>";
         return element;
     },
     notificationReceived: function () { },
