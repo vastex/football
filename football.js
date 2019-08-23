5a330b86735e7eaa6f486dde2bc72dfcc7d1ae55
vastex <37942727+vastex@users.noreply.github.com>
2019-08-24 00:52:35 +0200

    Update football.js

--- football.js
+++ football.js
@@ -1,25 +1,49 @@
 Module.register("football", {
     defaults: {},
-    start: function () { },
+    start: function () {
+        this.getStanding();
+        this.scheduleUpdate();
+    },
     getScripts: function () {
         return ["https://code.jquery.com/jquery-3.4.1.min.js"];
     },
     getDom: function () {
-        $.ajax({
-            headers: { 'X-Auth-Token': 'f808fe88377e4d759e8fcb1d6eaa27e9' },
-            url: "http://api.football-data.org/v2/competitions/2003/matches?matchday=1",
-            dataType: 'json',
-        type: 'GET',
-        }).done(function(response) {
-            // do something with the response, e.g. isolate the id of a linked resource   
-            console.log(response);
-        });
         var element = document.createElement("div");
         element.className = "myContent";
         element.innerHTML = "<table style='width:100%' id='standing'><tr><th>Heim</th><th>Gast</th><th>Ergebnis</th></tr><tr><td>BVB</td><td>Bayern</td><td>2:2</td></tr></table>";
         return element;
     },
+    scheduleUpdate: function (delay) {
+        var nextLoad = 60000;
+
+        var self = this;
+        setInterval(function () {
+            self.getStanding();
+        }, nextLoad);
+
+        
+    },
+    getStanding: function () {
+        $.ajax({
+            headers: { 'X-Auth-Token': 'f808fe88377e4d759e8fcb1d6eaa27e9' },
+            url: "http://api.football-data.org/v2/competitions/2003/matches",
+            dataType: 'json',
+            type: 'GET',
+        }).done(function (response) {
+            // do something with the response, e.g. isolate the id of a linked resource   
+            console.log(response);
+            $.each(response, function () {
+                $.each(this, function (name, value) {
+                    console.log(name + '=' + value);
+                });
+            });
+
+        });
+        this.sendSocketNotification('GET_TICKERS', url);
+    },
+
     notificationReceived: function () { },
     socketNotificationReceived: function () { },
+
     
 })
