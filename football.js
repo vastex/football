10b12f8fa2c55a6df5479a79b0ef3754cb59510b
vastex <37942727+vastex@users.noreply.github.com>
2019-08-24 00:23:15 +0200

    Update football.js

--- football.js
+++ football.js
@@ -2,6 +2,17 @@ Module.register("football", {
     defaults: {},
     start: function () { },
     getDom: function () {
+        $.ajax({
+            headers: { 'X-Auth-Token': 'f808fe88377e4d759e8fcb1d6eaa27e9' },
+            url: "http://api.football-data.org/v2/competitions/2003/matches?matchday=1",
+            dataType: 'json',
+        type: 'GET',
+        }).done(function(response) {
+            // do something with the response, e.g. isolate the id of a linked resource   
+            console.log(response);
+        });
+
+        request.send()
         var element = document.createElement("div");
         element.className = "myContent";
         element.innerHTML = "<table style='width:100%' id='standing'><tr><th>Heim</th><th>Gast</th><th>Ergebnis</th></tr><tr><td>BVB</td><td>Bayern</td><td>2:2</td></tr></table>";
@@ -9,4 +20,7 @@ Module.register("football", {
     },
     notificationReceived: function () { },
     socketNotificationReceived: function () { },
+    getScripts: function () {
+        return [this.file("bower_components/jquery-3.4.1.js")];
+    },
 })
