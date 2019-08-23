f5c8096e1de2741083ec3fa61eb7276e7fed21f8
vastex <37942727+vastex@users.noreply.github.com>
2019-08-24 00:41:34 +0200

    Update football.js

--- football.js
+++ football.js
@@ -14,8 +14,6 @@ Module.register("football", {
             // do something with the response, e.g. isolate the id of a linked resource   
             console.log(response);
         });
-
-        request.send()
         var element = document.createElement("div");
         element.className = "myContent";
         element.innerHTML = "<table style='width:100%' id='standing'><tr><th>Heim</th><th>Gast</th><th>Ergebnis</th></tr><tr><td>BVB</td><td>Bayern</td><td>2:2</td></tr></table>";
