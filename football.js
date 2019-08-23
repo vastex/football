ee51b9397c94aaefe7174821fe9c3a1d15626918
vastex <37942727+vastex@users.noreply.github.com>
2019-08-23 23:18:05 +0200

    Add files via upload

--- football.js
+++ football.js
@@ -7,7 +7,8 @@ Module.register("football", {
     // Override dom generator.
     getDom: function () {
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
+        wrapper.setAttribute("id", "tablediv");
+        wrapper.innerHTML = "<table style='width:100%' id='standing'><tr><th>Heim</th><th>Gast</th><th>Ergebnis</th></tr><tr><td>BVB</td><td>Bayern</td><td>2:2</td></tr></table>";
         return wrapper;
     }
 });
